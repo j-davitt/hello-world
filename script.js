27fc7a82983701fa1f31248b65678cb65cbd558e
Joe Davitt <joseph.j.davitt@gmail.com>
2022-11-17 18:55:31 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+alert('ANNOYING!');
+alert('STILL ANNOYING!');
+let usersName = prompt('What is your name?');
+if (confirm('All your base are belong to us ' + usersName)) {
+    document.write('BWAHAHAHA all your base are belong to us ' + usersName);
+} else {
+    document.write('I really wish all your base are belong to us ' + usersName);
+}
