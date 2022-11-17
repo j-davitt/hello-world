10e0f9a84791f89896903776db3b58dfeedc3311
Joe Davitt <joseph.j.davitt@gmail.com>
2022-11-17 19:33:07 +0000

    Update script

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 alert('ANNOYING!');
-alert('STILL ANNOYING!');
-let usersName = prompt('What is your name?');
-if (confirm('All your base are belong to us ' + usersName)) {
-    document.write('BWAHAHAHA all your base are belong to us ' + usersName);
-} else {
-    document.write('I really wish all your base are belong to us ' + usersName);
+
+    
+function pageWelcome(){
+    let usersName = prompt('What is your name?');
+    if (usersName == ''){
+        alert("You didn't type a name");
+        usersName = prompt('I must know your name. What is it?');
+    }
+    if (confirm('All your base are belong to us ' + usersName)) {
+        document.write('BWAHAHAHA all your base are belong to us ' + usersName);
+    } else {
+        document.write('I really wish all your base are belong to us ' + usersName);
+    }
 }
+
+
