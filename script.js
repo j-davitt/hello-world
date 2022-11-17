b95e578493c542433359f1acf5e950287d24f44a
Joe Davitt <joseph.j.davitt@gmail.com>
2022-11-17 23:00:34 +0000

    Update script

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
-    
-function pageWelcome(){
+
+function pageWelcome() {
     let usersName = prompt('What is your name?');
-    if (usersName == ''){
+    while (usersName == '' || usersName == null) {
         alert("You didn't type a name");
         usersName = prompt('I must know your name. What is it?');
     }
@@ -13,6 +13,18 @@ function pageWelcome(){
         document.write('Welcome ' + usersName + '. This is where your lucky number would be.... IF YOU HAD ONE!');
     }
 }
+function bourbonCounter() {
+    let bottleCount = prompt('1-5');
+    for (i = 0; i < bottleCount; i++) {
+        document.write('Bourbon');
+    }
+}
+
+
+    // let bottleCount = prompt('How many bottles of bourbon? 1-5');
+    // while (bottleCount != 1 || bottleCount != 2 || bottleCount != 3 || bottleCount != 4 || bottleCount != 5) {
+    //     bottleCount = prompt('Must be between 1-5. Try again');
+    // }
 
 
 // lucky number generator
